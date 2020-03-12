951110944ce0f6884d3a2d2419858bdb185f4a3e
majos95 <majos95@gmail.com>
2020-03-12 12:41:19 +0000

    Implements SubmitQuestionTest closes #13

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/SubmitQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/SubmitQuestionTest.groovy
@@ -19,15 +19,125 @@ import spock.lang.Specification
 @DataJpaTest
 class SubmitQuestionTest extends Specification {
 
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String ACRONYM = "AS1"
+    public static final String ACADEMIC_TERM = "1 SEM"
+    public static final String QUESTION_TITLE = 'question title'
+    public static final String QUESTION_CONTENT = 'question content'
+    public static final String OPTION_CONTENT = "optionId content"
+    public static final String STUDENT_NAME = "Student Name"
+    public static final String STUDENT_USERNAME = "StudentUsername"
+    public static final Integer STUDENT_KEY = 1
+
+
+    @Autowired
+    QuestionService questionService
+
+    @Autowired
+    Course course
+
+    @Autowired
+    User student
+
+    @Autowired
+    QuestionRepository questionRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    CourseExecution courseExecution
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+
+
+    def setup() {
+        course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+
+        courseExecution = new CourseExecution(course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        courseExecutionRepository.save(courseExecution)
+
+        student = new User(STUDENT_NAME, STUDENT_USERNAME, STUDENT_KEY, User.Role.STUDENT)
+        student.setId(STUDENT_KEY)
+
+    }
+
     def "submitted question must be pending and created"() {
-        expect: false
+        given: "A question"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_TITLE)
+        questionDto.setContent(QUESTION_CONTENT)
+        and: 'a optionId'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_CONTENT)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+        questionDto.setOptions(options)
+
+        when:
+        questionService.submitQuestion(student.getKey(), course.getId(), questionDto)
+
+        then:
+        questionRepository.count() == 1L
+        def submittedQuestion = questionRepository.findAll().get(0)
+        submittedQuestion.getId() != null
+        submittedQuestion.getKey() == 1
+        submittedQuestion.getStatus() == Question.Status.PENDING
+        submittedQuestion.getTitle() == QUESTION_TITLE
+        submittedQuestion.getContent() == QUESTION_CONTENT
+        submittedQuestion.getImage() == null
+        submittedQuestion.getOptions().size() == 1
+        submittedQuestion.getCourse().getName() == COURSE_NAME
+        course.getQuestions().contains(submittedQuestion)
     }
 
     def "user contains submitted question"() {
-        expect: false
+        given: "A question"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_TITLE)
+        questionDto.setContent(QUESTION_CONTENT)
+
+        when:
+        questionService.submitQuestion(student.getKey(), course.getId(), questionDto)
+
+        then:
+        questionRepository.count() == 1L
+        def submittedQuestion = questionRepository.findAll().get(0)
+        def submittedQuestions = student.getSubmittedQuestions()
+        submittedQuestions.contains(submittedQuestion)
     }
 
     def "submitted question must not be a null object"() {
-        expect: false
+        given: "A question"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_TITLE)
+        questionDto.setContent(QUESTION_CONTENT)
+
+        setup:
+        questionService.submitQuestion(student.getKey(), course.getId(), questionDto)
+        def submittedQuestion = questionRepository.findAll().get(0)
+
+        when:
+        submittedQuestion == null
+
+        then:
+        InvalidObjectException exception = thrown()
+        exception.message("Submitted question failed. Object is invalid")
+    }
+
+
+    @TestConfiguration
+    static class QuestionServiceImplTestContextConfiguration {
+
+        @Bean
+        QuestionService questionService() {
+            return new QuestionService()
+        }
     }
 }
