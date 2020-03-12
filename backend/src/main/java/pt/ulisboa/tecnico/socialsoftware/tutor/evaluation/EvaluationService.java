c0909f7a5aed3ae59eac525b86730b52b37dea53
majos95 <majos95@gmail.com>
2020-03-12 15:11:39 +0000

    Implements CreateEvaluationTest and SubmitEvaluationTest closes #17

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/evaluation/EvaluationService.java
@@ -0,0 +1,30 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.evaluation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Service
+public class EvaluationService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public EvaluationDto createEvaluation(EvaluationDto evaluationDto, int questionKey) {
+
+    }
+
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public void submitEvaluation(Integer questionId, boolean approvedEvaluation, String justification) {
+
+    }
+}
--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/evaluation/service/CreateEvaluationTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/evaluation/service/CreateEvaluationTest.groovy
@@ -1,23 +1,82 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.evaluation.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.evaluation.EvaluationDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.evaluation.EvaluationRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.evaluation.EvaluationService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import spock.lang.Specification
+
 
 @DataJpaTest
 class CreateEvaluationTest extends Specification {
 
+    @Autowired
+    Question pendingQuestion
+
+    @Autowired
+    EvaluationService evaluationService
+
+    @Autowired
+    EvaluationRepository evaluationRepository
+
+    def setup(){
+        pendingQuestion = new Question()
+        pendingQuestion.setKey(1)
+    }
+
     def "created evaluation can't be approved nor have justification"() {
-        //checks if created evaluation attributes are correctly instantiated
-        expect: false
+        given: "An evaluationDto"
+        def evaluationDto = new EvaluationDto()
+
+        when:
+        evaluationService.createEvaluation(evaluationDto, pendingQuestion.getKey())
+
+        then: "the correct evaluation is inside the repository"
+        evaluationRepository.count() == 1L
+        def evaluation = evaluationRepository.findAll().get(0)
+        evaluation.getEvaluation() == false
+        evaluation.getJustification() == null
     }
 
     def "evaluation has pending question"() {
-        //checks if pending evaluation is correctly assigned to a pending question
-        expect: false
+        given: "An evaluationDto"
+        def evaluationDto = new EvaluationDto()
+
+        when:
+        evaluationService.createEvaluation(evaluationDto, pendingQuestion.getKey())
+
+        then: "associated question must be pending"
+        def evaluation = evaluationRepository.findAll().get(0)
+        evaluation.getSubmittedQuestion().getStatus() == Question.Status.PENDING
     }
 
     def "evaluation not null"() {
-        //checks if object created is not null
-        expect: false
+        given: "An evaluationDto"
+        def evaluationDto = new EvaluationDto()
+
+        setup:
+        evaluationService.createEvaluation(evaluationDto, pendingQuestion.getKey())
+        def evaluation = evaluationRepository.findAll().get(0)
+
+        when:
+        evaluation == null
+
+        then:
+        InvalidObjectException exception = thrown()
+        exception.message("Evaluation creation failed. Object is invalid")
+
+    }
+
+    @TestConfiguration
+    static class EvaluationServiceImplTestContextConfiguration {
+
+        @Bean
+        EvaluationService evaluationService() {
+            return new EvaluationService()
+        }
     }
 }
