b4b30e15d09c66bca8fd2eb2f6d69c2080c222e8
JTCaldeira <jt.brazaocaldeira@gmail.com>
2020-03-13 16:14:39 +0000

    Define tests closes #66

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/ShowOpenTournamentsServiceSpockTest.groovy
@@ -0,0 +1,40 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.*
+
+@DataJpaTest
+class ShowOpenTournamentServiceSpockTest extends Specification {
+
+	def "show tournaments in whose course execution the user is not enrolled"() {
+		// throw exception
+		expect: false
+	}
+
+	def "show tournaments associated with the user's course execution"() {
+		// throw exception
+		expect: false
+	}
+}
