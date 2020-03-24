ec4c20d3d1c4a6478d4b9052af544000c0e7082d
JTCaldeira <jt.brazaocaldeira@gmail.com>
2020-03-24 18:39:11 +0000

    Define webservice closes #102

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -4,11 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class TournamentController {
 
     @Autowired
     private TournamentService tournamentService;
+
+    @PostMapping("/executions/{executionId}/students/{studentId}/")
+    @PreAuthorize("hasRole('STUDENT') and hasPermission(#executionId, 'EXECUTION.ACCESS')")
+    public void createTournament(@PathVariable int studentId, @PathVariable int executionId,
+                                 @RequestBody List<String> topics, @RequestBody TournamentDto tournamentDto) {
+        tournamentService.createTournament(studentId, executionId, topics, tournamentDto);
+    }
 
     @PostMapping("/tournament/enroll/{studentId}/{tournamentId}")
     @PreAuthorize("hasRole('STUDENT')")
