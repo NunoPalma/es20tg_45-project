2df3f6c0a3c063a416efe588d03b03128f59f7df
majos95 <majos95@gmail.com>
2020-03-25 15:19:25 +0000

    Implements Feature Test of SubmitEvaluation

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/evaluation/EvaluationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/evaluation/EvaluationController.java
@@ -2,7 +2,6 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.evaluation;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
 
 import javax.validation.Valid;
 
@@ -15,7 +14,8 @@ public class EvaluationController {
 
     @PutMapping("/evaluations/{questionId}")
     @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#questionId, 'QUESTION.ACCESS')")
-    public EvaluationDto submitEvaluation(@Valid @RequestBody QuestionDto question, @RequestParam boolean approved, @RequestParam String justification){
-        return this.evaluationService.submitEvaluation(question, approved, justification);
+    public EvaluationDto submitEvaluation(@Valid @RequestBody EvaluationDto evaluationDto, @PathVariable Integer questionId){
+        return this.evaluationService.submitEvaluation(evaluationDto, questionId);
     }
 }
+
