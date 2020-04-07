2b3c050d6e26b4cf30f5e6709c25c9eb4874f4da
NunoPalma <nunopalma98@gmail.com>
2020-04-07 20:10:47 +0100

    Implement TournamentsView enroll student's templates closes #141

--- frontend/src/views/student/TournamentsView.vue
+++ frontend/src/views/student/TournamentsView.vue
@@ -12,7 +12,6 @@
 				loading-text="Loading tournaments"
 		>
 			<template v-slot:top>
-
 				<v-card-title>
 					<v-text-field
 							v-model="search"
@@ -25,36 +24,6 @@
 					>New Tournament</v-btn
 					>
 				</v-card-title>
-			</template>
-
-			<template v-slot:item.action="{ item }">
-				<v-tooltip bottom>
-					<template v-slot:activator="{ on }">
-						<v-icon
-								small
-								class="mr-2"
-								v-on="on"
-								@click="createFromCourse(item)"
-								data-cy="createFromCourse"
-						>cached</v-icon
-						>
-					</template>
-					<span>Create from Course</span>
-				</v-tooltip>
-				<v-tooltip bottom>
-					<template v-slot:activator="{ on }">
-						<v-icon
-								small
-								class="mr-2"
-								v-on="on"
-								@click="deleteCourse(item)"
-								color="red"
-								data-cy="deleteCourse"
-						>delete</v-icon
-						>
-					</template>
-					<span>Delete Course</span>
-				</v-tooltip>
 			</template>
 
 			<template v-slot:item.name="{item}">
@@ -71,20 +40,32 @@
 
 			<template v-slot:item.creator="{item}">
 				<v-layout align="left">
-						{{item.creator.name}}
+					{{item.creator.name}}
 				</v-layout>
 			</template>
 
 			<template v-slot:item.startDate="{item}">
 				<v-layout align="left">
-						{{item.startDate}}
+					{{item.startDate}}
 				</v-layout>
 			</template>
 
 			<template v-slot:item.endDate="{item}">
 				<v-layout align="left">
-						{{ item.endDate }}
+					{{ item.endDate }}
 				</v-layout>
+			</template>
+
+			<template v-slot:item.enrolled="{item}">
+				<v-btn
+						:id=item.tournamentId
+						v-on:click="onEnrolledButtonClicked(item)"
+						:class="isEnrolled(item.tournamentId) ? 'green' : 'red'"
+						align="left"
+						x-small
+						fab=true
+				>
+				</v-btn>
 			</template>
 
 		</v-data-table>
@@ -106,6 +87,7 @@
 	import Tournament from '@/models/management/Tournament';
 	import RemoteServices from '@/services/RemoteServices';
 	import EditTournamentDialog from '@/views/student/EditTournamentDialog.vue';
+
 	@Component({
 		components: {
 			'edit-tournament-dialog': EditTournamentDialog
@@ -131,15 +113,21 @@
 				width: '10%'
 			},
 			{
+				text: 'End Date',
+				value: 'endDate',
+				align: 'left',
+				width: '10%'
+			},
+			{
 				text: 'Status',
 				value: 'status',
 				align: 'left',
 				width: '10%'
 			},
 			{
-				text: 'Actions',
-				value: 'action',
-				align: 'center',
+				text: 'Enrolled',
+				value: 'enrolled',
+				align: 'left',
 				sortable: false,
 				width: '10%'
 			}
@@ -153,74 +141,30 @@
 			}
 			await this.$store.dispatch('clearLoading');
 		}
+
 		newTournament() {
 			this.currentCourse = new Course();
 			this.editTournamentDialog = true;
 		}
-		/*
-=======
-            {
-                text: 'Status',
-                value: 'status',
-                align: 'left',
-                width: '10%'
-            },
-            {
-                text: 'Actions',
-                value: 'action',
-                align: 'center',
-                sortable: false,
-                width: '10%'
-            }
-        ];
-        async created() {
-            await this.$store.dispatch('loading');
-            try {
-            	this.tournaments = await RemoteServices.getAvailableTournaments();
-            } catch (error) {
-                await this.$store.dispatch('error', error);
-            }
-            await this.$store.dispatch('clearLoading');
-        }
-        newTournament() {
-            this.currentCourse = new Course();
-            this.editTournamentDialog = true;
-        }
-        /*
->>>>>>> Stashed changes
-        createFromCourse(course: Course) {
-            this.currentCourse = new Course(course);
-            this.currentCourse.courseExecutionId = undefined;
-            this.currentCourse.courseExecutionType = 'EXTERNAL';
-            this.currentCourse.acronym = undefined;
-            this.currentCourse.academicTerm = undefined;
-            this.editCourseDialog = true;
-        }
-        */
+
 		async onCreateTournament(tournament: Tournament) {
 			this.tournaments.unshift(tournament);
 			this.editTournamentDialog = false;
 			this.currentCourse = null;
 		}
+
 		onCloseDialog() {
 			this.editTournamentDialog = false;
 			this.currentCourse = null;
 		}
-		/*
-        async deleteCourse(courseToDelete: Course) {
-            if (confirm('Are you sure you want to delete this question?')) {
-                try {
-                    await RemoteServices.deleteCourse(courseToDelete.courseExecutionId);
-                    this.courses = this.courses.filter(
-                        course => course.courseExecutionId != courseToDelete.courseExecutionId
-                    );
-                } catch (error) {
-                    await this.$store.dispatch('error', error);
-                }
-            }
-        }
-         */
+
+		isEnrolled(tournamentId: number) {
+		}
+
+		onEnrolledButtonClicked(tournament: Tournament) {
+
+		}
 	}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped></style> <!--TODO create new webservices => user is enrolled in tournament-->
