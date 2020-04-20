b2bf765018d59e0e63529a4e8ffafbac94a43a5a
DiogoLobato <diogo.lobato@tecnico.ulisboa.pt>
2020-04-20 14:51:38 +0100

    Fixes display

--- frontend/src/views/teacher/evaluations/EvaluateQuestionDialog.vue
+++ frontend/src/views/teacher/evaluations/EvaluateQuestionDialog.vue
@@ -8,14 +8,14 @@ o<template>
         <span class="headline">{{question.title}}</span>
       </v-card-title>
 
-      <v-card-text class="text-left">
+      <v-card-text class="text-left" v-if="question">
         <div>
           <span v-html="convertMarkDown(question.content, question.image)" />
           <ul>
             <li v-for="option in question.options" :key="option.number">
               <span
-                v-if="option.correct"
-                v-html="convertMarkDown('**[★]** ', null)"
+                      v-if="option.correct"
+                      v-html="convertMarkDown('**[★]** ', null)"
               />
               <span
                       v-html="convertMarkDown(option.content, null)"
@@ -29,9 +29,9 @@ o<template>
 
       <v-card-text class="text-left">
         <v-switch
-          v-model="statusDefault"
-          label="Question Approved"
-          />
+                v-model="statusDefault"
+                label="Question Approved"
+        />
       </v-card-text>
 
       <v-card-text class="text-left">
@@ -50,92 +50,92 @@ o<template>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Model } from 'vue-property-decorator';
-import Question from '../../../models/management/Question';
-import Evaluation from '@/models/management/Evaluation';
-import RemoteServices from '@/services/RemoteServices';
-import Image from '@/models/management/Image';
-import { convertMarkDown } from '@/services/ConvertMarkdownService';
+  import { Component, Prop, Vue, Model } from 'vue-property-decorator';
+  import Question from '../../../models/management/Question';
+  import Evaluation from '@/models/management/Evaluation';
+  import RemoteServices from '@/services/RemoteServices';
+  import Image from '@/models/management/Image';
+  import { convertMarkDown } from '@/services/ConvertMarkdownService';
 
-@Component
-export default class EvaluateQuestionDialog extends Vue {
-  @Prop({type: Question, required: true}) readonly question!: Question;
-  @Model('dialog', Boolean) dialog!: boolean;
+  @Component
+  export default class EvaluateQuestionDialog extends Vue {
+    @Prop({type: Question, required: true}) readonly question!: Question;
+    @Model('dialog', Boolean) dialog!: boolean;
 
-  evaluation!: Evaluation;
-  editQuestion!: Question;
-  statusDefault = false;
-  justification = '';
+    evaluation!: Evaluation;
+    editQuestion!: Question;
+    statusDefault = false;
+    justification = '';
 
-  async created() {
-    this.evaluation = new Evaluation(await RemoteServices.findEvaluation(this.question));
-    this.editQuestion = this.question;
-  }
-
-  // https://github.com/F-loat/vue-simplemde/blob/master/doc/configuration_en.md
-  markdownConfigs: object = {
-    status: false,
-    spellChecker: false,
-    insertTexts: {
-      image: ['![image][image]', '']
+    async created() {
+      this.evaluation = new Evaluation(await RemoteServices.findEvaluation(this.question));
+      this.editQuestion = this.question;
     }
-  };
 
-  async setStatus(status: boolean) {
-    try {
-      if(status){
-        if(this.question.id){
-          let questionNew = await RemoteServices.setQuestionStatus(this.question.id, 'AVAILABLE');
-          if (questionNew) {
-            this.editQuestion.status = 'AVAILABLE';
+    // https://github.com/F-loat/vue-simplemde/blob/master/doc/configuration_en.md
+    markdownConfigs: object = {
+      status: false,
+      spellChecker: false,
+      insertTexts: {
+        image: ['![image][image]', '']
+      }
+    };
+
+    async setStatus(status: boolean) {
+      try {
+        if(status){
+          if(this.question.id){
+            let questionNew = await RemoteServices.setQuestionStatus(this.question.id, 'AVAILABLE');
+            if (questionNew) {
+              this.editQuestion.status = 'AVAILABLE';
+            }
           }
         }
-      }
-      else{
-        if(this.question.id){
-          let questionNew = await RemoteServices.setQuestionStatus(this.question.id, 'REJECTED');
-          if (questionNew) {
-            this.editQuestion.status = 'REJECTED';
+        else{
+          if(this.question.id){
+            let questionNew = await RemoteServices.setQuestionStatus(this.question.id, 'REJECTED');
+            if (questionNew) {
+              this.editQuestion.status = 'REJECTED';
+            }
           }
         }
-      }
 
-    } catch (error) {
-      await this.$store.dispatch('error', error);
-    }
-  }
-
-  convertMarkDown(text: string, image: Image | null = null): string {
-    return convertMarkDown(text, image);
-  }
-
-  closeDialogue() {
-    this.$emit('close-evaluate-question-dialog');
-  }
-
-  async evaluateQuestion() {
-    if(this.evaluation && (this.statusDefault === false && (this.justification === '' || this.justification === null))){
-      await this.$store.dispatch('error', 'Rejected Question must have a justification!');
-      return;
-    }
-    if(this.evaluation && this.question){
-      try{
-        if(this.statusDefault === true){
-          this.evaluation.approvedEvaluation = true;
-        }
-        else if(this.statusDefault === false){
-          this.evaluation.approvedEvaluation = false;
-        }
-        await this.setStatus(this.statusDefault);
-        this.evaluation.justification = this.justification;
-        const result = await RemoteServices.submitEvaluation(this.evaluation, this.question);
-        this.$emit('evaluate-question', result);
       } catch (error) {
         await this.$store.dispatch('error', error);
       }
     }
+
+    convertMarkDown(text: string, image: Image | null = null): string {
+      return convertMarkDown(text, image);
+    }
+
+    closeDialogue() {
+      this.$emit('close-evaluate-question-dialog');
+    }
+
+    async evaluateQuestion() {
+      if(this.evaluation && (this.statusDefault === false && (this.justification === '' || this.justification === null))){
+        await this.$store.dispatch('error', 'Rejected Question must have a justification!');
+        return;
+      }
+      if(this.evaluation && this.question){
+        try{
+          if(this.statusDefault === true){
+            this.evaluation.approvedEvaluation = true;
+          }
+          else if(this.statusDefault === false){
+            this.evaluation.approvedEvaluation = false;
+          }
+          await this.setStatus(this.statusDefault);
+          this.evaluation.justification = this.justification;
+          const result = await RemoteServices.submitEvaluation(this.evaluation, this.question);
+          this.$emit('evaluate-question', result);
+        } catch (error) {
+          await this.$store.dispatch('error', error);
+        }
+      }
+    }
   }
-}
 </script>
 
 <style scoped></style>
