cba7b74c15cec5d576c0217ad1ca3dcb9e068b66
UnlimitedPugWorks <42717736+UnlimitedPugWorks@users.noreply.github.com>
2020-05-07 01:50:47 +0100

    Added features and refactoring in page

--- frontend/src/views/student/DiscussionsView.vue
+++ frontend/src/views/student/DiscussionsView.vue
@@ -1,81 +1,89 @@
 <template>
-  <v-card class="table">
-    <v-data-table
-      :headers="headers"
-      :items="discussions"
-      :search="search"
-      multi-sort
-      :mobile-breakpoint="0"
-      :items-per-page="15"
-      :footer-props="{ itemsPerPageOptions: [15, 30, 50, 100] }"
-    >
-      <template v-slot:top>
-        <v-card-title>
-          <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            class="mx-2"
-          />
-        </v-card-title>
-      </template>
+  <div>
+    <div class="stats-container">
+    <div class="items">
+      <div class="icon-wrapper" ref="discussionsMade">
+        <animated-number :number="discussions.length">
+          {{discussions.length}}</animated-number>
+      </div>
+      <div class="project-name">
+        <p>Total Discussions Made</p>
+      </div>
+    </div>
+      <div class="items">
+        <div class="icon-wrapper" ref="discussionsMade">
+          <animated-number :number="discussions.length">
+            {{discussions.filter(disc => disc.status==='OPEN').length}}</animated-number>
+        </div>
+        <div class="project-name">
+          <p>Total Open Discussions</p>
+        </div>
+      </div>
+    </div>
+    <v-card class="table">
+      <v-data-table
+        :headers="headers"
+        :items="discussions"
+        :search="search"
+        multi-sort
+        :mobile-breakpoint="0"
+        :items-per-page="15"
+        :footer-props="{ itemsPerPageOptions: [15, 30, 50, 100] }"
+      >
+        <template v-slot:top>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              class="mx-2"
+            />
+          </v-card-title>
+        </template>
 
-      <template v-slot:item.questionTitle="{ item }">
-        <v-chip small>
-          <span>{{ item.questionTitle }}</span>
-        </v-chip>
-      </template>
+        <template v-slot:item.questionTitle="{ item }">
+          <v-chip small>
+            <span>{{ item.questionTitle }}</span>
+          </v-chip>
+        </template>
 
-      <template v-slot:item.title="{ item }">
-        <v-chip @click="seeDiscussion(item)" small>
-          <span>{{ item.title }}</span>
-        </v-chip>
-      </template>
+        <template v-slot:item.title="{ item }">
+          <v-chip @click="seeDiscussion(item)" small>
+            <span>{{ item.title }}</span>
+          </v-chip>
+        </template>
 
-      <template v-slot:item.status="{ item }">
-        <v-chip :color="getStatusColor(item)" small>
-          <span>{{ item.postsDto[item.postsDto.length - 1].status }}</span>
-        </v-chip>
-      </template>
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="getStatusColor(item)" small>
+            <span>{{ item.status }}</span>
+          </v-chip>
+        </template>
 
-      <template v-slot:item.creationDate="{ item }">
-        {{ item.postsDto[0].creationDate }}
-      </template>
+        <template v-slot:item.creationDate="{ item }">
+          {{ item.postsDto[0].creationDate }}
+        </template>
 
-      <template v-slot:item.action="{ item }">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-icon small class="mr-2" v-on="on" @click="seeDiscussion(item)"
-              >visibility</v-icon
-            >
-          </template>
-          <span>Show Doubt</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-icon
-              small
-              class="mr-2"
-              v-on="on"
-              @click="deleteItem(item)"
-              color="red"
-              data-cy="deleteQuestion"
-              >delete</v-icon
-            >
-          </template>
-          <span>Delete Question</span>
-        </v-tooltip>
-      </template>
-    </v-data-table>
-    <see-discussion-dialog
-      v-if="discussion"
-      v-model="seeDiscussionDialog"
-      :discussion="discussion"
-      :id="discussion.id"
-      v-on:see-discussion="onSeeDiscussion"
-      v-on:close-dialog="onCloseDialog"
-    />
-  </v-card>
+        <template v-slot:item.action="{ item }">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon small class="mr-2" v-on="on" @click="seeDiscussion(item)"
+                >visibility</v-icon
+              >
+            </template>
+            <span>Show Doubt</span>
+          </v-tooltip>
+        </template>
+      </v-data-table>
+      <see-discussion-dialog
+        v-if="discussion"
+        v-model="seeDiscussionDialog"
+        :discussion="discussion"
+        :id="discussion.id"
+        v-on:see-discussion="onSeeDiscussion"
+        v-on:close-dialog="onCloseDialog"
+      />
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -83,6 +91,8 @@ import { Component, Vue } from 'vue-property-decorator';
 import RemoteServices from '@/services/RemoteServices';
 import Discussion from '@/models/management/Discussion';
 import SeeDiscussionDialog from '@/views/student/SeeDiscussionDialog.vue';
+import AnimatedNumber from '@/components/AnimatedNumber.vue';
+
 @Component({
   components: {
     'see-discussion-dialog': SeeDiscussionDialog
@@ -113,8 +123,7 @@ export default class DoubtView extends Vue {
   }
 
   getStatusColor(discussion: Discussion) {
-    status = discussion.postsDto[discussion.postsDto.length - 1].status;
-    if (status === 'UNSOLVED') return 'red';
+    if (discussion.status !== 'OPEN') return 'red';
     else return 'green';
   }
 
@@ -135,6 +144,53 @@ export default class DoubtView extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.stats-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+  align-content: center;
+  height: 100%;
+
+  .items {
+    background-color: rgba(255, 255, 255, 0.75);
+    color: #1976d2;
+    border-radius: 5px;
+    flex-basis: 25%;
+    margin: 20px;
+    cursor: pointer;
+    transition: all 0.6s;
+  }
+}
+
+.project-name {
+  align-self: start;
+}
+.project-name p {
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: 2px;
+  transform: translateY(0px);
+  transition: all 0.5s;
+}
+
+.icon-wrapper,
+.project-name {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon-wrapper {
+  font-size: 100px;
+  transform: translateY(0px);
+  transition: all 0.6s;
+}
+
+.icon-wrapper {
+  align-self: end;
+}
 .container {
   max-width: 1000px;
   margin-left: auto;
