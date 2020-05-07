4bece36e7b230c19f6a6b0176fac8a6b415ff1ec
NunoPalma <nunopalma98@gmail.com>
2020-05-07 16:40:00 +0100

    Define GetStudentTournamentStats tests closes #251

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/GetStudentTournamentStatsServiceSpockTest.groovy
@@ -0,0 +1,47 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.Tournament
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDateTime
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.*
+
+@DataJpaTest
+class GetStudentTournamentStatsServiceSpockTest extends Specification {
+
+	def setup() {
+	}
+
+	def "get student stats with no recorded stats"() {
+		expect: false
+	}
+
+	def "get student stats with recorded stats"() {
+		expect: false
+	}
+
+	@TestConfiguration
+	static class ServiceImplTestContextConfiguration {
+
+		@Bean
+		TournamentService tournamentService() {
+			return new TournamentService()
+		}
+	}
+}
